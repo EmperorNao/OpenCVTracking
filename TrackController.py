018c13cdd99ca95adfa6d39ed7b97fe73750e85c
EmperorNao <asgasgagagwet@gmail.com>
2021-04-15 13:10:01 +0300

    Updated

--- TrackController.py
+++ TrackController.py
@@ -8,38 +8,30 @@ THRESHOLD = 200
 
 global points
 points = []
+window_name = "output"
 
 
 def add_point(event, x, y, flags, param):
-    """
-    callback function that append coordinates for point to global array points
-
-    :param event:
-    :param x:
-    :param y:
-    :param flags:
-    :param param:
-    :return:
-    """
 
     if event == cv2.EVENT_LBUTTONDBLCLK:
 
-        points.append([x, y])
+        param.append([x, y])
 
 
-def choose_reference_points_to_frame(window_name, frame):
+def choose_reference_points_to_frame(window_name_l, frame):
 
-    cv2.setMouseCallback(window_name, add_point())
+    p = []
+    cv2.setMouseCallback(window_name_l, add_point, p)
 
     while True:
 
-        cv2.imshow(window_name, frame)
-        if cv2.waitKey() == ord('q'):
+        cv2.imshow(window_name_l, frame)
+        if cv2.waitKey() == ord('v'):
 
             break
 
-    cv2.setMouseCallback(window_name, lambda *args: None)
-    return points
+    cv2.setMouseCallback(window_name_l, lambda *args: None)
+    return p
 
 
 # this function was_depricates
@@ -134,28 +126,27 @@ def show_with_contours(path, show=True, log=False, blacked=False, vectors=False,
 '''
 
 
-
 # TODO
-#  1) remake function and make her ables to stop video and choose contours in time
-#  2) remake callback without global variable
-
+#  1) remake callback without global variable
 def track(path,
-                                    show=True,
-                                    track=True,
-                                    log=False,
-                                    blacked=False,
-                                    vectors=False,
-                                    n_vectors=6,
-                                    sleep = 0,
-                                    timing = False,
-                                    track_log = True):
+          show=True,
+          is_track=True,
+          log=False,
+          blacked=False,
+          vectors=False,
+          n_vectors=6,
+          sleep=0,
+          timing=False,
+          track_log=True):
 
     tracker = CentroidTracker()
     cap = cv2.VideoCapture(path)
     ret = True
 
-    cv2.namedWindow("output", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("output", 900, 700)
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, 900, 700)
+
+    points_to_control = []
 
     while ret:
 
@@ -166,41 +157,41 @@ def track(path,
             print("Can't receive frame (stream end?). Exiting ...")
             break
 
-        hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        thresh = hsvImage
         # convert image in binary
+        hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        thresh = hsv_image
+        # threshold controls how many objects and with which precision we will find
         _, thresh = cv2.threshold(thresh, THRESHOLD, 255, cv2.THRESH_BINARY_INV)
 
-
-        # find and draw contour
+        # find contour
         contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cv2.drawContours(frame, contours, -1, (255, 0, 0), 3, cv2.LINE_AA, hierarchy, 1)
 
         centroids = []
-        for contour in contours:
+        for i in range(0, len(contours)):
 
-            if (len(contour) > n_vectors):
+            if len(contours[i]) > n_vectors:
 
-                M = cv2.moments(contour)
-                cX = int(M["m10"] / (M["m00"] + 0.0001))
-                cY = int(M["m01"] / (M["m00"] + 0.0001))
-                centroids.append([cX, cY])
+                m = cv2.moments(contours[i])
+                c_x = int(m["m10"] / (m["m00"] + 0.0001))
+                c_y = int(m["m01"] / (m["m00"] + 0.0001))
+                centroids.append([c_x, c_y])
+                i += 1
 
         objects = tracker.update(centroids)
 
         # find and draw center
         for contour in contours:
 
-            if (len(contour) > n_vectors):
+            if len(contour) > n_vectors:
 
-                M = cv2.moments(contour)
-                cX = int(M["m10"] / (M["m00"] + 0.0001))
-                cY = int(M["m01"] / (M["m00"] + 0.0001))
-                cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+                m = cv2.moments(contour)
+                c_x = int(m["m10"] / (m["m00"] + 0.0001))
+                c_y = int(m["m01"] / (m["m00"] + 0.0001))
+                cv2.circle(frame, (c_x, c_y), 5, (255, 255, 255), -1)
 
                 start_time = time.perf_counter()
-
-                vector_ends = get_vectors(contour, n_vectors, cX, cY)
+                vector_ends = get_vectors(contour, n_vectors, c_x, c_y)
 
                 if timing:
 
@@ -215,19 +206,18 @@ def track(path,
                     for p in vector_ends:
 
                         # draw vectors from center
-                        cv2.line(frame, (cX, cY), (p[0], p[1]), (0, 128, 0), 3)
+                        cv2.line(frame, (c_x, c_y), (p[0], p[1]), (0, 128, 0), 3)
 
+                    # draw m-param contour
                     for i in range(0, len(vector_ends) - 1):
 
-                        # draw m-param contour
                         cv2.line(frame, (vector_ends[i][0], vector_ends[i][1]), (vector_ends[i + 1][0], vector_ends[i + 1][1]), (0, 0, 255), 3)
 
-
                     l = len(vector_ends)
-                    if (l > 0):
+                    if l > 0:
                         cv2.line(frame, (vector_ends[0][0], vector_ends[0][1]), (vector_ends[l - 1][0], vector_ends[l - 1][1]), (0, 0, 255), 3)
 
-        if track:
+        if is_track:
             for (objectID, centroid) in objects.items():
                 # draw both the ID of the object and the centroid of the
                 # object on the output frame
@@ -237,25 +227,27 @@ def track(path,
                 cv2.circle(frame, (centroid[0], centroid[1]), 5, (0, 255, 0), -1)
 
             if track_log:
-
-                print("Number of trackable objects:", len(objects.items()))
+                print("Number of tracked objects:", len(objects.items()))
 
         if show:
             if blacked:
-
-                cv2.imshow("output", thresh)
+                cv2.imshow(window_name, thresh)
 
             else:
+                cv2.imshow(window_name, frame)
 
-                cv2.imshow('output', frame)
-
-            waitkey = cv2.waitKey(sleep)
-            if waitkey == ord('q'):
+            key = cv2.waitKey(sleep)
+            if key == ord('q'):
                 break
-            elif waitkey == ord('s'):
+
+            elif key == ord('s'):
                 sleep = 0
-            elif waitkey == ord('g'):
+
+            elif key == ord('g'):
                 sleep = 1
+
+            elif key == ord('v'):
+                points_to_control = choose_reference_points_to_frame(window_name, frame)
 
     cap.release()
     cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from TrackController import track
 
 path = r'TwoObjectsTest_.mp4'
 
-track(path, n_vectors=24, sleep=0, vectors=True, track=True, show=True, track_log=False)
+track(path, n_vectors=24, sleep=0, vectors=True, is_track=True, show=True, track_log=False)
